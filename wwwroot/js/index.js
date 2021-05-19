8cb998416871c36ca3d446ffd7b9776a3a217c5c
Haakon Langballe <65386118+Haakonl61@users.noreply.github.com>
2021-05-19 13:05:48 +0200

    MVC

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,12 +1,23 @@
-﻿console.log("Hallo!");
+﻿$(document).ready(function () {
+    console.log("Hallo!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-    button.addEventListener("click", function () {
-    console.log("Buying item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying item");
     });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item(0).children;
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    })
+});
